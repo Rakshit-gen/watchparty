3115a670f92fabc111018026b37354326db7c6ad
Rakshit-gen <64471316+Rakshit-gen@users.noreply.github.com>
2025-10-27 22:00:06 +0530

    backend port

--- watchparty-backend/server.js
+++ watchparty-backend/server.js
@@ -1,7 +1,10 @@
 const WebSocket = require('ws');
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Watch Party WebSocket Server is running');
+});
 
 const wss = new WebSocket.Server({ server });
 
